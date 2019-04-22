1daf2863ad629cd0e369d1bd4be060a099681c4f
Jan S <jan@americano.site>
2019-04-22 06:47:52 +0100

    simplest form with echo response

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Listonosz' });
 });
+router.post('/',(req,res)=>{
+  res.render('index',{title:"Listonosz",response:JSON.stringify(req.body)})
+})
 
 module.exports = router;
