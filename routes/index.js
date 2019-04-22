c0a1e425ec0efc7671118ab01b20726aac07b5af
Jan S <jan@americano.site>
2019-04-22 06:56:54 +0100

    axios added and makes request to echo server on 3333

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Listonosz' });
 });
-router.post('/',(req,res)=>{
-  res.render('index',{title:"Listonosz",response:JSON.stringify(req.body)})
+router.post('/',async (req,res)=>{
+  console.log("Got body",req.body)
+  let response=await axios(req.body);
+  res.render('index',{title:"Listonosz",response:JSON.stringify(response.data)})
 })
 
 module.exports = router;
