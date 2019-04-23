544664827bf731e52d7c92f6b4f12b83e716972f
Jan S <jan@americano.site>
2019-04-23 12:26:18 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
+const Sequelize=require('sequelize');
+const sequelize=new Sequelize('sqlite://db.sqlite')
+const Request=sequelize.define('request',{
+  method:{
+    type:Sequelize.STRING,
+    allowNull:false      
+  },
+  url:{
+    type:Sequelize.STRING,
+    allowNull:false
+  },
+  response:{
+    type:Sequelize.TEXT,
+  },
+  responseCode:{
+    type:Sequelize.INTEGER,
+    allowNull:false
+  }
+},{})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,11 +36,17 @@ router.post('/',async (req,res)=>{
   let error;
   try {
     response=await axios(request);
+    storeInDB(response,error,request)
   }
   catch (e){
     error=e;
   }
   res.render('index',{title:"Listonosz",response,error,request})
 })
+function storeInDB(response,error,request){
+  Request.create({method:request.method,url:request.url,responseCode:response.status,response:JSON.stringify(response.data)}).then(()=>{
+    sequelize.sync();
+  })
+}
 
 module.exports = router;
