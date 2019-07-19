e5be7ca84698f86028667c245972bd528e6ca18e
Jan Skowronski <crianonim@gmail.com>
2019-07-19 08:38:21 +0100

    Replace jest with tape

--- client/src/tests/helper.test.js
+++ client/src/tests/helper.test.js
@@ -1,9 +1,13 @@
 const helper = require('../lib/helper');
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(helper.sum(1, 2)).toBe(3);
+const test = require('tape')
+test('adds 1 + 2 to equal 3', (t) => {
+  t.equal(helper.sum(1, 2),3);
+  t.end();
 });
 
-// test('convertObjectIntoSortedArray to return an array',()=>{
-//   expect()
-// })
+test('convertObjectIntoSortedArray to return an array',(t)=>{
+    t.equal(Array.isArray( helper.convertObjectIntoSortedArray({b:12,c:"JAN","a":3})),true)
+    t.end();
+})
+
+// helper.convertObjectIntoSortedArray({b:12,c:"JAN","a":3})
