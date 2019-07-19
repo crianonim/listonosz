817150d1a0ea955f8e2403d58e39e9a898f2a64e
Jan Skowronski <crianonim@gmail.com>
2019-07-19 08:14:55 +0100

    Initial testing setup with jest

---
+++ client/src/tests/helper.test.js
@@ -0,0 +1,9 @@
+const helper = require('../lib/helper');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(helper.sum(1, 2)).toBe(3);
+});
+
+// test('convertObjectIntoSortedArray to return an array',()=>{
+//   expect()
+// })
